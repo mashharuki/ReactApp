a1e42b0326758e36b5d4b527162ce7b44ab435b8
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-04-19 21:20:38 +0900

    リンクを接続する。

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-let counter = 0;
 let dom = document.querySelector('#root');
-// 関数呼び出し
-doCount ();
 
-// 関数 
-function doCount () {
-    counter++;
-    let element = React.createElement(
-        'p', {}, 'count:' + counter
-    )
-    // レンダリング
-    ReactDOM.render(element, dom)
-}
+let element = React.createElement(
+    'div', {}, [
+        React.createElement(
+            'h2', {}, "Hello!"
+        ),
+        React.createElement(
+            'h4', {className: 'alert alert-primary'}, 
+            "React sample page."
+        ),
+        React.createElement(
+            'ul', {className: 'list-group'}, [
+                React.createElement(
+                    'li', {className: 'list-group-item'}, 
+                    "First item."
+                ),
+                React.createElement(
+                    'li', {className: 'list-group-item'}, 
+                    "Second item."
+                ),
+                React.createElement(
+                    'li', {className: 'list-group-item'}, 
+                    "Thrid item."
+                ),
+            ]
+        ),
+    ])
+
+let el = (
+    <div>
+        <h2>
+            JSX sample
+        </h2>
+        <p>
+            This is sample message.
+        </p>
+    </div>
+)
+    
+// レンダリング
+ReactDOM.render(el, dom);
